b1ec46533ea08f1dcda0185553a209556223a525
sumitvijaykumar <sumit2408@gmail.com>
2018-09-03 23:39:46 +0530

    sysout test

--- src/test/java/com/twitterapi/automation/resource/Specifications.java
+++ src/test/java/com/twitterapi/automation/resource/Specifications.java
@@ -18,6 +18,7 @@ public class Specifications {
 	
 	public static AuthenticationScheme getAuthScheme(){
 		AuthenticationScheme authScheme;
+		System.out.println(System.getenv("HOME"));
 		authScheme = RestAssured.oauth(Credentials.CONSUMER_KEY, Credentials.CONSUMER_SECRET, Credentials.ACCESS_TOKEN, Credentials.ACCESS_SECRET);
 		return authScheme;
 	}
